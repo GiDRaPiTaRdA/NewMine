63d223b41fbd30c849990f5bc6d52101ef9c3184
Karlash Maksim <xxmaxdrivezz@gmail.com>
2018-12-18 23:33:42 +0200

    build block with out of chunk

--- Assets/Scripts/BlockInteraction.cs
+++ Assets/Scripts/BlockInteraction.cs
@@ -2,70 +2,117 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockInteraction : MonoBehaviour {
+public class BlockInteraction : MonoBehaviour
+{
 
-	public GameObject cam;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0))
+    public GameObject cam;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
             RaycastHit hit;
-            
+
             //for mouse clicking
             //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-   			//if ( Physics.Raycast (ray,out hit,10)) 
-   			//{
-            
-   			//for cross hairs
-            if (Physics.Raycast(this.cam.transform.position, this.cam.transform.forward, out hit, 10))
+            //if ( Physics.Raycast (ray,out hit,10)) 
+            //{
+
+            //for cross hairs
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 10))
             {
-   				Vector3 hitBlock = hit.point - hit.normal/2.0f; 
+                Chunk hitc;
+                if (!World.chunks.TryGetValue(hit.collider.gameObject.transform.position, out hitc)) return;
 
-   				int x = (int) (Mathf.Round(hitBlock.x) - hit.collider.gameObject.transform.position.x);
-   				int y = (int) (Mathf.Round(hitBlock.y) - hit.collider.gameObject.transform.position.y);
-   				int z = (int) (Mathf.Round(hitBlock.z) - hit.collider.gameObject.transform.position.z);
+                Vector3 hitBlock;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    hitBlock = hit.point - hit.normal / 2.0f;
 
-   				List<Vector3> updates = new List<Vector3>();
-   				float thisChunkx = hit.collider.gameObject.transform.position.x;
-   				float thisChunky = hit.collider.gameObject.transform.position.y;
-   				float thisChunkz = hit.collider.gameObject.transform.position.z;
+                }
+                else
+                    hitBlock = hit.point + hit.normal / 2.0f;
 
-   				updates.Add(hit.collider.gameObject.transform.position);
+                int x = (int)(Mathf.Round(hitBlock.x) - hit.collider.gameObject.transform.position.x);
+                int y = (int)(Mathf.Round(hitBlock.y) - hit.collider.gameObject.transform.position.y);
+                int z = (int)(Mathf.Round(hitBlock.z) - hit.collider.gameObject.transform.position.z);
 
-   				//update neighbours?
-   				if(x == 0) 
-   					updates.Add(new Vector3(thisChunkx-World.chunkSize,thisChunky,thisChunkz));
-				if(x == World.chunkSize - 1) 
-					updates.Add(new Vector3(thisChunkx+World.chunkSize,thisChunky,thisChunkz));
-				if(y == 0) 
-					updates.Add(new Vector3(thisChunkx,thisChunky-World.chunkSize,thisChunkz));
-				if(y == World.chunkSize - 1) 
-					updates.Add(new Vector3(thisChunkx,thisChunky+World.chunkSize,thisChunkz));
-				if(z == 0) 
-					updates.Add(new Vector3(thisChunkx,thisChunky,thisChunkz-World.chunkSize));
-				if(z == World.chunkSize - 1) 
-					updates.Add(new Vector3(thisChunkx,thisChunky,thisChunkz+World.chunkSize));
+                bool update = false;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //update = hitc.chunkData[x, y, z].HitBlock();
 
-	   			foreach(Vector3 cname in updates)
-	   			{
-	   				Chunk c;
-					if(World.chunks.TryGetValue(cname, out c))
-					{
-						DestroyImmediate(c.chunk.GetComponent<MeshFilter>());
-						DestroyImmediate(c.chunk.GetComponent<MeshRenderer>());
-						DestroyImmediate(c.chunk.GetComponent<Collider>());
-						c.chunkData[x,y,z].SetType(Block.BlockType.AIR);
-				   		c.DrawChunk();
-			   		}
-			   	}
-		   	}
-   		}
-	}
+                    Block block = hitc.chunkData[x, y, z];
+
+                    block.SetType(Block.BlockType.AIR);
+
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<MeshFilter>());
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<MeshRenderer>());
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<Collider>());
+                    hitc.DrawChunk();
+
+                    update = true;
+                }
+                else
+                {
+                    Block block = hitc.chunkData[x, y, z];
+                    block.SetType(Block.BlockType.STONE);
+
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<MeshFilter>());
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<MeshRenderer>());
+                    GameObject.DestroyImmediate(hitc.chunk.GetComponent<Collider>());
+                    hitc.DrawChunk();
+
+                    update = true;
+
+                }
+
+                if (update)
+                {
+
+                    List<Vector3> updates = new List<Vector3>();
+                    float thisChunkx = hitc.chunk.transform.position.x;
+                    float thisChunky = hitc.chunk.transform.position.y;
+                    float thisChunkz = hitc.chunk.transform.position.z;
+
+                    //updates.Add(hit.collider.gameObject.name);
+
+                    //update neighbours?
+                    if (x == 0)
+                        updates.Add(new Vector3(thisChunkx - World.chunkSize, thisChunky, thisChunkz));
+                    if (x == World.chunkSize - 1)
+                        updates.Add(new Vector3(thisChunkx + World.chunkSize, thisChunky, thisChunkz));
+                    if (y == 0)
+                        updates.Add(new Vector3(thisChunkx, thisChunky - World.chunkSize, thisChunkz));
+                    if (y == World.chunkSize - 1)
+                        updates.Add(new Vector3(thisChunkx, thisChunky + World.chunkSize, thisChunkz));
+                    if (z == 0)
+                        updates.Add(new Vector3(thisChunkx, thisChunky, thisChunkz - World.chunkSize));
+                    if (z == World.chunkSize - 1)
+                        updates.Add(new Vector3(thisChunkx, thisChunky, thisChunkz + World.chunkSize));
+
+                    foreach (Vector3 cname in updates)
+                    {
+                        if (World.chunks.TryGetValue(cname, out Chunk c))
+                        {
+
+                            GameObject.DestroyImmediate(c.chunk.GetComponent<MeshFilter>());
+                            GameObject.DestroyImmediate(c.chunk.GetComponent<MeshRenderer>());
+                            GameObject.DestroyImmediate(c.chunk.GetComponent<Collider>());
+                            c.DrawChunk();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
